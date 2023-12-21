0cfeef7dbe6535ea9d868d34287478d7f1670fca
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2023-12-21 16:47:26 +0100

    css reset, placeholder components, and Plant Hero!

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import PlantHero from './components/icons/PlantHero.vue';
 </script>
 
 <template>
-
+  <PlantHero />
 </template>
 
 <style scoped>
---
+++ src/components/icons/PlantHero.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <img src="@/assets/svg/logo.svg" alt="The PP logo" />
+        <img src="@/assets/svg/seedling.svg" alt="A seedling" />
+        <img src="@/assets/svg/dig.svg" alt="A digging shovel" />
+    </div>
+</template>
+
+<style scoped>
+div {
+    display: grid;
+    @media screen and (min-width: 700px) {
+        grid-template: 256px 256px / 512px 1fr;
+    }
+}
+img {
+    object-fit: none;
+    width: 100%;
+    height: 100%;
+}
+img:nth-child(1) {
+    background: center / cover url("@/assets/img/succulent.avif");
+    grid-row: 2 span;
+    @media screen and (max-width: 699px) {
+        grid-column: 2 span;
+    }
+}
+img:nth-child(2) {
+    background-color: var(--brown);
+}
+img:nth-child(3) {
+    background-color: var(--green);
+}
+img:nth-child(n + 2) {
+    @media screen and (max-width: 699px) {
+        padding: 15%;
+        object-fit: contain;
+    }
+}
+</style>
