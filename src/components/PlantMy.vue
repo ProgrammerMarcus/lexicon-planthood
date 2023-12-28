2206eab1f932bb523e0dbc5a54f6204781600f79
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2023-12-28 09:12:38 +0100

    added header thing on new page

---
+++ src/components/PlantMy.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <img src="@/assets/svg/logo.svg" alt="The PP logo" />
+        <h1>Mina sidor</h1>
+    </div>
+</template>
+
+<style scoped>
+div {
+    display: grid;
+    background: center / cover url("@/assets/img/hand.avif");
+    place-items: center;
+    place-content: center;
+    gap: 30px;
+    height: 390px;
+}
+img {
+    object-fit: none;
+}
+h1 {
+    color: white;
+    font-weight: 600;
+    margin: 0;
+}
+</style>
--- src/components/PlantPlans.vue
+++ src/components/PlantPlans.vue
@@ -1,13 +1,14 @@
 <script setup>
+import PlantMy from './PlantMy.vue';
 import PlantNavbar from './PlantNavbar.vue'
 import PlantReturn from './PlantReturn.vue';
 </script>
 
 <template>
   <PlantNavbar />
+  <PlantMy />
   <PlantReturn />
 </template>
 
 <style scoped>
-
 </style>
