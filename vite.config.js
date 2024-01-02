6513e71cbdf8d02c1a15a3be96050aa7aa74417d
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2024-01-02 14:37:54 +0100

    teeeest

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,8 +10,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('/src', import.meta.url))
     }
   },
-  base: ''
+  base: '/lexicon-planthood/'
 })
