1bc9bd419bfd1d138c1ce1fcfe8e9611da1c5f4a
Marcus <125326139+ProgrammerMarcus@users.noreply.github.com>
2024-01-02 14:32:13 +0100

    imported images, probably fixes prod

--- src/assets/plants.js
+++ src/assets/plants.js
@@ -1,36 +1,43 @@
+import cucumberUrl from "./img/cucumber.avif"
+import tomatoesUrl from "./img/tomatoes.avif"
+import tomatoUrl from "./img/tomato.avif"
+import peaUrl from "./img/pea.avif"
+import turnipUrl from "./img/turnip.avif"
+import squashUrl from "./img/squash.avif"
+
 const plants = [
     {
-        image: "src/assets/img/cucumber.avif",
+        image: cucumberUrl,
         item: "Gurka",
         date: "15 april 2024",
         producer: "Rico Coolio",
     },
     {
-        image: "src/assets/img/tomatoes.avif",
+        image: tomatoesUrl,
         item: "Tomat",
         date: "1 april 2024",
         producer: "Jocke",
     },
     {
-        image: "src/assets/img/tomato.avif",
+        image: tomatoUrl,
         item: "Tomat",
         date: "1 april 2024",
         producer: "Majvor och P-Å",
     },
     {
-        image: "src/assets/img/pea.avif",
+        image: peaUrl,
         item: "Sockerärtor",
         date: "15 april 2024",
         producer: "Frippe",
     },
     {
-        image: "src/assets/img/turnip.avif",
+        image: turnipUrl,
         item: "Majrova",
         date: "1 april 2024",
         producer: "Emma på 4an",
     },
     {
-        image: "src/assets/img/squash.avif",
+        image: squashUrl,
         item: "Squash",
         date: "15 april 2024",
         producer: "Perra J",
